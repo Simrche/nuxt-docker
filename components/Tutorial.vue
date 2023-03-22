376205e8780addb96ef836271213d301e4548356
Simrche <simon.roche@edu.devinci.fr>
2023-03-22 14:33:42 +0100

    init

---
+++ components/Tutorial.vue
@@ -0,0 +1,12 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="flex h-full w-full justify-center items-center">
+    <h1>Coucou</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NuxtTutorial",
+};
+</script>
